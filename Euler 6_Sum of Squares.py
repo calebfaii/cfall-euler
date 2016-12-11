426d189f8356b3d899454e2fd55c4bd205d10c03
Caleb Fall <caleb.a.fall@gmail.com>
2016-12-11 14:58:49 -0500

    Update and rename Euler 6.py to Euler 6_Sum of Squares.py

--- Euler 6_Sum of Squares.py
+++ Euler 6_Sum of Squares.py
@@ -1,6 +1,5 @@
 ##Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 
-
 def SumOfSquares(value):
     sum_squares = 0
     for i in range(1,(value + 1)):
