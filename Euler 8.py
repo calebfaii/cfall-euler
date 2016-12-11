dff6c0628eeae1b450ae79eb859f566f22f78897
Caleb Fall <caleb.a.fall@gmail.com>
2016-12-11 15:01:06 -0500

    Update Euler 8.py

--- Euler 8.py
+++ Euler 8.py
@@ -1,4 +1,5 @@
-##NOTE TO SELF - IT WORKS!
+##The four adjacent digits in the 1000-digit number that have the greatest product are 9 x 9 x 8 x 9 = 5832.
+##Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
 
 from operator import mul
 
