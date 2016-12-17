6c95456e0ce33766ba75813a71f555e386fd008c
Caleb Fall <caleb.a.fall@gmail.com>
2016-12-17 16:15:13 -0500

    Testing Pycharm integration.

--- Euler 22_Names Scores.py
+++ Euler 22_Names Scores.py
@@ -2,10 +2,8 @@
 ##Then working out the alphabetical value for each name, multiply this value by its alphabetical 
 ##position in the list to obtain a name score.
 ##For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, 
-##is the 938th name in the list. So, COLIN would obtain a score of 938 × 53 = 49714.
+##is the 938th name in the list. So, COLIN would obtain a score of 938 x 53 = 49714.
 ##What is the total of all the name scores in the file?
-
-# Test
 
 import time
 start = time.time()
