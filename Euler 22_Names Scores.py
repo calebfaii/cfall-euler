567d1a21f932718ade57868a6cd5ba134ea17bdb
Caleb Fall <caleb.a.fall@gmail.com>
2016-12-17 16:16:08 -0500

    Testing Pycharm integration.

--- Euler 22_Names Scores.py
+++ Euler 22_Names Scores.py
@@ -5,6 +5,7 @@
 ##is the 938th name in the list. So, COLIN would obtain a score of 938 x 53 = 49714.
 ##What is the total of all the name scores in the file?
 
+
 import time
 start = time.time()
 names = []
