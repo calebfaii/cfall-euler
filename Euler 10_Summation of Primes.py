eb159076aa71bfec6d3cf0fcbc2e20306b6366bb
Caleb Fall <caleb.a.fall@gmail.com>
2017-01-06 10:02:06 -0500

    Testing Pycharm integration.

---
+++ Euler 10_Summation of Primes.py
@@ -0,0 +1,19 @@
+def primes_sieve(limit):
+    limitn = limit+1
+    not_prime = set()
+    primes = []
+
+    for i in range(2, limitn):
+        if i in not_prime:
+            continue
+
+        for f in range(i*2, limitn, i):
+            not_prime.add(f)
+
+        primes.append(i)
+
+    return primes
+
+primes = primes_sieve(2000000)
+x = sum(lprimes)
+print x
--- Euler 22_Names Scores.py
+++ Euler 22_Names Scores.py
@@ -1,4 +1,5 @@
-##Using p022_names.txt, a 46K text file containing over five-thousand first names, begin by sorting it into alphabetical order. 
+##Using p022_names.txt, a 46K text file containing over five-thousand first names, begin by sorting it into alphabetical
+#  order.
 ##Then working out the alphabetical value for each name, multiply this value by its alphabetical 
 ##position in the list to obtain a name score.
 ##For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, 
